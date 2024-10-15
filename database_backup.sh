74bcdec4deefab7e6e65587685161556f17d61b6
aia89 <aiadosbaeva@gmail.com>
2024-10-15 10:30:50 -0500

    Database Backup Script

---
+++ database_backup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+database_name="your_database"
+output_file="database_backup_$(date +%Y%m%d).sql"
+mysqldump -u username -ppassword "$database_name" > "$output_file"
+echo "Database backup created: $output_file"
