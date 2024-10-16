2e0b0b5f85bc0ce61e9be037864075b35a5d8e52
aia89 <aiadosbaeva@gmail.com>
2024-10-16 10:42:13 -0500

    User Password Expiry Checker Script

---
+++ user_passwd_exp_checker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+IFS=$'\n'
+for user in $(cat /etc/passwd | grep "/bin/bash" | cut -d: -f1); do
+password_expires=$(chage -l "$user" | grep "Password expires" | awk '{print $4}')
+echo "User: $user, Password Expires: $password_expires"
+done
+unset IFS
